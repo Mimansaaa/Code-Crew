a6f706cff8bbc0580e6495453f7dbf3e29f79751
aanchal <tailwalaanhal@gmail.com>
2023-08-19 23:39:09 +0530

    checking Alex

---
+++ polybot/app.py
@@ -0,0 +1,45 @@
+import telebot
+from loguru import logger
+import os
+import requests
+from collections import Counter
+class Bot:
+
+    def __init__(self, token):
+        self.bot = telebot.TeleBot(token, threaded=False)
+        self.bot.set_update_listener(self._bot_internal_handler)
+
+        self.current_msg = None
+
+    def _bot_internal_handler(self, messages):
+        """Bot internal messages handler"""
+        for message in messages:
+            self.current_msg = message
+            self.handle_message(message)
+
+    def start(self):
+        """Start polling msgs from users, this function never returns"""
+        logger.info(f'{self.__class__.__name__} is up and listening to new messages....')
+        logger.info(f'Telegram Bot information\n\n{self.bot.get_me()}')
+
+        self.bot.infinity_polling()
+
+    def send_text(self, text):
+        self.bot.send_message(self.current_msg.chat.id, text)
+
+    def send_text_with_quote(self, text, message_id):
+        self.bot.send_message(self.current_msg.chat.id, text, reply_to_message_id=message_id)
+
+    def handle_message(self, message):
+        """Bot Main message handler"""
+        logger.info(f'Incoming message: {message}')
+        self.send_text(f'Your original message: {message.text}')
+
+if __name__ == '__main__':
+    # TODO - in the 'polyBot' dir, create a file called .telegramToken and store your bot token there.
+    #  ADD THE .telegramToken FILE TO .gitignore, NEVER COMMIT IT!!!
+    with open('.telegramToken') as f:
+        _token = f.read()
+
+    my_bot = Bot(_token)
+    my_bot.start()
