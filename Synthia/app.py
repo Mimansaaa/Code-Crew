921431887ad1fc6cebc561183bcdab5996eac24c
aanchal <tailwalaanhal@gmail.com>
2023-08-27 16:52:13 +0530

    motivational func

--- Synthia/app.py
+++ Synthia/app.py
@@ -1,14 +1,21 @@
-import sqlite3
 import telebot
-from loguru import logger
+import random
+import schedule
+import threading
+import time
+import sqlite3
 import openai
+from loguru import logger
+import traceback
+
 
 class Bot:
-    def _init_(self, token):
+    def __init__(self, token):
         self.bot = telebot.TeleBot(token, threaded=False)
         self.bot.set_update_listener(self._bot_internal_handler)
 
         self.current_msg = None
+        self.user_details = {}
 
     def _bot_internal_handler(self, messages):
         """Bot internal messages handler"""
@@ -17,22 +24,20 @@ class Bot:
             self.handle_message(message)
 
     def start(self):
-        """Start polling msgs from users, this function never returns"""
-        logger.info(
-            f"{self._class.name_} is up and listening to new messages...."
-        )
+        logger.info(f"{self.__class__.__name__} is up and listening to new messages....")
         logger.info(f"Telegram Bot information\n\n{self.bot.get_me()}")
 
         self.bot.infinity_polling()
 
-    def send_text(self, text):
-        self.bot.send_message(self.current_msg.chat.id, text)
-
+    def send_text(self, text, chat_id=None):
+        if chat_id is None:
+            chat_id = self.current_msg.chat.id
+        return self.bot.send_message(chat_id, text)
 
     def handle_message(self, message):
-        """Bot Main message handler"""
         logger.info(f"Incoming message: {message}")
         self.send_text(f"Your original message: {message.text}")
+
 
 class QuoteBot(Bot):
     def handle_message(self, message):
@@ -40,26 +45,39 @@ class QuoteBot(Bot):
 
 
 class SustainabilityBot(Bot):
+    def __init__(self, token, openai_key):
+        super().__init__(token)
+        self.openai_key = openai_key
+
     def search_gpt(self, query):
-        # Generate a response
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": query}],
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.7,
-        )
-
-        response = completion.choices[0].message.content
-
+        response = self.generate_response(query)
         return response
 
+    def generate_response(self, prompt):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=150,
+            api_key=self.openai_key
+        )
+        return response.choices[0].text.strip()
+
     def handle_message(self, message):
-        logger.info(f"Incoming message: {message}")
+        super().handle_message(message)
 
         if message.text.startswith('/feedback'):
             self.send_feedback_button(message)
+        elif message.text.startswith('/search'):
+            self.handle_chatgpt(message)
+        elif message.text.startswith('/stop'):
+            self.handle_stop(message)
+
+    def handle_chatgpt(self, message):
+        user_input = message.text.replace("/search", "").strip()
+        education_prompt = "You are a chatbot specialized in discussing education topics. Please provide information about educational methods, learning strategies, online courses, or any other educational subject:"
+        full_prompt = education_prompt + " " + user_input
+        chatbot_response = self.search_gpt(full_prompt)
+        self.send_text(chatbot_response, message.chat.id)
 
     def send_feedback_button(self, message):
         markup = telebot.types.InlineKeyboardMarkup()
@@ -73,13 +91,46 @@ class SustainabilityBot(Bot):
             reply_markup=markup,
         )
 
+    def start_motivational_thread(self):
+        # Implement your motivational thread here
+        pass
 
-if __name__ == "_main_":
-    _token = "6333049326:AAGZH5kjbQwKMSWWNldjkspmE18Eg6s8tTw"
-    openai.api_key = "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"
-    my_bot = SustainabilityBot(_token)
+    def stop_motivational_thread(self):
+        # Implement stopping the motivational thread here
+        pass
 
-    conn = sqlite3.connect('feedback.db')
+    def start(self):
+        super().start()
+        self.start_motivational_thread()
+
+        self.schedule = schedule.Scheduler()
+        self.schedule.every(4).seconds.do(self.send_motivational_message)
+
+        self.bot_thread = threading.Thread(target=self.schedule_loop)
+        self.bot_thread.start()
+
+    def schedule_loop(self):
+        try:
+            while True:
+                self.schedule.run_pending()
+                time.sleep(5)
+        except Exception as e:
+            traceback.print_exc()
+
+    def stop(self):
+        self.stop_motivational_thread()
+        super().stop()
+
+
+if __name__ == "__main__":
+    with open(".telegramToken2", "r") as telegram_token_file:
+        _token = telegram_token_file.read().strip()
+
+    # Load the GPT token from the file
+    with open(".gptkey", "r") as gpt_token_file:
+        openai_key = gpt_token_file.read().strip()
+
+    my_bot = SustainabilityBot(_token, openai_key)
 
     conn = sqlite3.connect('feedback.db')
     cursor = conn.cursor()
@@ -96,22 +147,60 @@ if __name__ == "_main_":
         feedback_text = message.text
 
         try:
+            # Connect to the SQLite database and create a cursor
+            conn = sqlite3.connect('feedback.db')
+            cursor = conn.cursor()
+
+            # Create the 'feedback' table if it doesn't exist
+            cursor.execute('''
+                            CREATE TABLE IF NOT EXISTS feedback (
+                                user_id INTEGER,
+                                feedback_text TEXT,
+                                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+                            )
+                        ''')
+
             cursor.execute("INSERT INTO feedback (user_id, feedback_text) VALUES (?, ?)", (user_id, feedback_text))
             conn.commit()
+
+            cursor.close()
+            conn.close()
 
             my_bot.bot.send_message(message.chat.id, "Thank you for your feedback!")
 
         except sqlite3.Error as e:
             my_bot.bot.send_message(message.chat.id,
-                                    "An error occurred while saving your feedback Please try again later.")
+                                    "An error occurred while saving your feedback. Please try again later.")
 
-    # Add a handler for voice messages
-    @my_bot.bot.message_handler(content_types=['voice'])
-    def handle_voice_message(message):
-        my_bot.handle_voice_message(message)
+
+    @my_bot.bot.message_handler(commands=["motivation"])
+    def handle_motivation(message):
+        chat_id = message.chat.id
+        if chat_id not in my_bot.user_details:
+            my_bot.user_details[chat_id] = {
+                'name': None,
+                'state': 'waiting_for_name'
+            }
+            my_bot.send_text("Hi! We'll keep motivating you to come and study with me.")
+            my_bot.send_text("So, what's your name?", chat_id)
+        else:
+            my_bot.send_text("You're already registered for motivational messages!")
+
+
+    @my_bot.bot.message_handler(
+        func=lambda message: my_bot.user_details.get(message.chat.id, {}).get('state') == 'waiting_for_name')
+    def receive_name(message):
+        chat_id = message.chat.id
+        my_bot.user_details[chat_id]['name'] = message.text
+        my_bot.user_details[chat_id]['state'] = 'registered'
+        my_bot.send_text("Thank you, {}. You're all set.".format(my_bot.user_details[chat_id]['name']), chat_id)
+        my_bot.start_motivational_thread()  # Start the motivational thread after name is received
+
+
     @my_bot.bot.message_handler(commands=["start"])
     def handle_start(message):
-        my_bot.send_text("Welcome to the SynthiaAI Bot. Click on /help to get started.")
+        def handle_start(message):
+            my_bot.send_text("Welcome to the SynthiaAI Bot. Click on /help to get started.")
 
 
     @my_bot.bot.message_handler(commands=["help"])
@@ -127,17 +216,14 @@ if __name__ == "_main_":
         my_bot.send_text(help_text)
 
 
-    @my_bot.bot.message_handler(commands=["search"])
-    def handle_chatgpt(message):
-        # Extract the query by removing '/chatgpt' from the start of the message text
-        query = message.text.replace("/search", "").strip()
-        # Call the gpt() function with the extracted query
-        response = my_bot.search_gpt(query)
-
-        my_bot.send_text_with_quote(response, message_id=message.message_id)
+    @my_bot.bot.message_handler(commands=["stop"])
+    def handle_stop(message):
+        my_bot.send_text("Stopping the bot...")
+        my_bot.stop()
 
 
     my_bot.start()
+
     # Close the SQLite connection when the bot stops polling
     cursor.close()
     conn.close()
--- Synthia/main.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-openai.api_key = ""
-
-audio_file = open("audiofile.mp3", "rb")
-transcript = openai.Audio.translate("whisper-1", audio_file)
-transcribed_text = transcript['text']
-
-print(transcribed_text)
